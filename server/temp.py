267f2d9c56a45e44b350517f2538e57fd6c8690f
YUDAEUN <udu5771@naver.com>
2021-11-18 23:44:38 +0900

    라즈베리파이에 dht11 연결해서 온습도값 받아오기

---
+++ server/temp.py
@@ -0,0 +1,13 @@
+import adafruit_dht
+from board import *
+
+# GPIO4
+SENSOR_PIN = D4
+
+dht11 = adafruit_dht.DHT11(SENSOR_PIN, use_pulseio=False)
+
+temperature = dht11.temperature
+humidity = dht11.humidity
+
+print(f"Humidity= {humidity:.2f}")
+print(f"Temperature= {temperature:.2f}°C")
