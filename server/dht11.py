69dacb0a33b9d8fc0d9223ead2764b0abe1b66cc
YUDAEUN <udu5771@naver.com>
2021-12-09 02:24:34 +0900

    socket 통신 시도중

--- server/dht11.py
+++ server/dht11.py
@@ -1,10 +1,10 @@
+# server2.py : 계속 보낼값 입력해 보낼 수 있음.
+import socket
 import time
 import adafruit_dht
 import threading
 import os
-import glob
 from board import *
-
 # GPIO4
 SENSOR_PIN = D4
 
@@ -26,30 +26,47 @@ def get_dht():
             			print('Failed')
 		time.sleep(0.5)
 
-
 def server():
-	host='192.168.0.7'
-	port=22
+	global temperature
+	connection=False
+	host = '192.168.137.155' # Symbolic name meaning all available interfaces
+	port = 8091 # Arbitrary non-privileged port
+ 
+
 	server_sock = socket.socket(socket.AF_INET)
 	server_sock.bind((host, port))
 	server_sock.listen(1)
-	print("waiting..")
-	out_data = int(10)
 
-	while True: 
-		client_sock, addr = server_sock.accept()
-		if client_sock: 
-			print('Connected by...', addr) 
-			in_data = client_sock.recv(1024) 
-			print('receive data :', in_data.decode("utf-8"), len(in_data)) 
-			while in_data :
-				client_sock.send(str(out_data).encode("utf-8")) 
-				print('send temperature :', temperature)
-				time.sleep(2)
+	print("waiting...")
+	client_sock, addr = server_sock.accept()
+
+	print('Connected by', addr)
+	
+
+	if(connection==False):
+		client_sock, client_info=server_sock.accept()
+		connection=True
+		print("connected")
+	
+
+	data = client_sock.recv(1024)
+	print(data.decode("utf-8"), len(data))
+
+
+	while(True):
+		data2 = int(temperature)
+		#print(data2.encode())
+		#client_sock.send(data)
+		#client_sock.send(data2.to_bytes(4, byteorder='little'))
+		i = 2
+		if input_string == "send":  # 휴대폰에서 send 라는 값이 올경우 ...라는 값을 돌려준다.
+			input_string = "..."
+    # 값하나 보냄(사용자가 입력한 숫자)
+		client_sock.send(str(data2).encode("utf-8"))
+		client_info.sendall(data2.encode("utf-8"))
 
 	client_sock.close()
 	server_sock.close()
-
 
 
 dht_thread=threading.Thread(target=get_dht)
--- server/temp.py
+++ server/temp.py
@@ -1,13 +1,77 @@
+import socket
+import time
 import adafruit_dht
+import threading
+import os
 from board import *
-
-# GPIO4
 SENSOR_PIN = D4
 
+GPIO.setmode(GPIO.BCM)
+
 dht11 = adafruit_dht.DHT11(SENSOR_PIN, use_pulseio=False)
+temperature=0
+humidity=0
 
-temperature = dht11.temperature
-humidity = dht11.humidity
+def get_dht():
+	global temperature
+	global humidity
 
-print(f"Humidity= {humidity:.2f}")
-print(f"Temperature= {temperature:.2f}°C")
+	while True:
+		try:
+			temperature = dht11.temperature
+			humidity = dht11.humidity
+			print(f"Humidity= {humidity:.2f}")
+			print(f"Temperature= {temperature:.2f}°C")
+		except RuntimeError:
+            			print('Failed')
+		time.sleep()
+
+
+def server():
+    HOST = "192.168.137.155"
+    PORT = 8091
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ('Socket created')
+    s.bind((HOST, PORT))
+    print ('Socket bind complete')
+    s.listen(1)
+    print ('Socket now listening')
+    
+
+
+
+    while True:
+    
+     #접속 승인
+        conn, addr = s.accept()
+        print("Connected by ", addr)
+
+    #데이터 수신
+        data = conn.recv(1024)
+        data = data.decode("utf8").strip()
+        if not data: break
+        print("Received: " + data)
+
+    #수신한 데이터로 파이를 컨트롤 
+        if input_string == "send":  # 휴대폰에서 send 라는 값이 올경우 ...라는 값을 돌려준다.
+            input_string = "..."
+
+        else:
+            input_string = input_string + " 없는 명령어 입니다."
+        print("파이 동작 :" + res)
+        conn.sendall(res.encode("utf-8"))
+
+    #연결 닫기
+    s.close()
+
+
+
+dht_thread=threading.Thread(target=get_dht)
+server_thread=threading.Thread(target=server)
+dht_thread.daemon=True
+server_thread.daemon=True
+server_thread.start()
+dht_thread.start()
+server_thread.join()
+dht_thread.join()
+print("end...")
